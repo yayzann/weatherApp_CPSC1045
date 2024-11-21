a5f67609e4ddf725da23b109dc4bf15300fb5b22
Yazan Ardekany <36682923+yayzann@users.noreply.github.com>
2024-11-20 17:07:18 -0800

    im tired man no msg here

--- weatherApp/main.js
+++ weatherApp/main.js
@@ -1,13 +1,51 @@
 onButton.addEventListener('click',fullMode);
 //offButton.addEventListener('click',testMode);
 
+async function getWeather() {
+    const key = '2afbfcd3d8b24fdf89f213443241711'
+    const city = 'Vancouver';
+    const url = 'http://api.weatherapi.com/v1/current.json?key=2afbfcd3d8b24fdf89f213443241711&q=$Vancouver';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+}
 
+const width = window.innerWidth-175
+const height = window.innerHeight-575
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.style.position = 'relative';
+canvas.style.top = '20px';
+canvas.style.left = '40px'
 function fullMode() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
     ctx.fillStyle = 'black'
     ctx.beginPath();
-    ctx.rect(0,0,900,400);
+    ctx.rect(0,0,width,height);
     ctx.closePath();
-    ctx.fill();
+    ctx.stroke();
+    getWeather();
+}
+
+function interpretCode() {
+
+}
+
+function offlineCodeGen() {
+    
+}
+
+function drawSunny() {
+
+}
+
+function drawRainy() {
+
+}
+
+function drawOvercast() {
+
+}
+
+function drawSnow() {
+
 }
