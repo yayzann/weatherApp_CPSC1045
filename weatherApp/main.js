fcbae5bb1640f17a8de0c230b6119cc03e800bd5
AverageTrout <36682923+AverageTrout@users.noreply.github.com>
2024-11-21 20:43:22 -0800

    i am exhausted man

--- weatherApp/main.js
+++ weatherApp/main.js
@@ -1,17 +1,17 @@
 onButton.addEventListener('click',fullMode);
 //offButton.addEventListener('click',testMode);
-
 async function getWeather() {
     const key = '2afbfcd3d8b24fdf89f213443241711'
     const city = 'Vancouver';
-    const url = 'http://api.weatherapi.com/v1/current.json?key=2afbfcd3d8b24fdf89f213443241711&q=$Vancouver';
+    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
     const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
+    let data = await response.json();
+    //console.log(data)
+    return data;
 }
 
-const width = window.innerWidth-175
-const height = window.innerHeight-575
+const width = window.innerWidth-1
+const height = window.innerHeight-1
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.style.position = 'relative';
@@ -23,12 +23,18 @@ function fullMode() {
     ctx.rect(0,0,width,height);
     ctx.closePath();
     ctx.stroke();
-    getWeather();
+    interpretCode();
 }
 
-function interpretCode() {
-
+async function interpretCode() {
+    const data = await getWeather();
+    const intData = {
+        code: data.current.condition.code,
+        temp_c: data.current.temp_c 
+    }
+    console.log(JSON.stringify(intData, null, 2));
 }
+
 
 function offlineCodeGen() {
     
