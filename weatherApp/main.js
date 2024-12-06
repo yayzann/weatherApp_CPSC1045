f97e69ee8952919e4885cf1c77bc426bf7ec8571
AverageTrout <36682923+AverageTrout@users.noreply.github.com>
2024-12-05 19:29:19 -0800

    fixing a LOT of ui stuff

--- weatherApp/main.js
+++ weatherApp/main.js
@@ -1,25 +1,31 @@
 onButton.addEventListener('click',fullMode);
 offButton.addEventListener('click',offlineCodeGen);
-//offButton.addEventListener('click',testMode);
 async function getWeather() { // interaction with API
     const key = '093a6a79ca9649b09ae40739240412' //this needs to be removed from git.
     const city = 'Vancouver'; //hardcoded cuz im hoping no one outside van is testing this, probably will switch to variable
     const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
     const response = await fetch(url);
     let data = await response.json();
-    console.log(data)
+    console.log(data);
     return data;
 }
 let weatherCodes = [1000,1003,1006,1009,1030,1063,1069,
     1072,1087,1114,1117,1135,1147,1150,1153,1168,1171,1180,1183,
     1186,1189,1192,1195,1201,1204,1207,1120,1213,1216,1219,1222,1225,1237,
-    1240,1243,1246,1249,1252,1255,1258,1261,1264,1273,1276,1279,1282]; //index of all possible weather codes
-const width = window.innerWidth
-const height = window.innerHeight
+    1240,1243,1246,1249,1252,1255,1258,1261,1264,1273,1276,1279,1282]; //index of all possible weather codes    
+//const width = window.innerWidth
+//const height = window.innerHeight
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.style.position = 'relative';
-canvas.style.top = '20px';
+//canvas.style.position = 'relative';
+//canvas.style.top = '20px';
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+const backdrop = new Image();
+backdrop.src = 'assets/backdrop.png';
+backdrop.onload = function () {
+    ctx.drawImage(backdrop, 0, 0, canvas.width, canvas.height);
+};
 
 function fullMode() { //operating with online functionality / full functionality of webapp
     interpretCodes();
@@ -86,9 +92,11 @@ async function interpretCodes() { //interprets weather codes recieved from API, 
             drawSnow();
             break;
     }
+    ctx.fillStyle = 'bisque';
+    ctx.font = "72px techno";
+    ctx.fillText(Math.round(intData.temp_c)+'c',(canvas.width/2)-72,canvas.height/2);
+
 }
-
-
 
 function offlineCodeGen() { //offline debugging mode, also here in case person grading assignment is offline
     switch (weatherCodes[(Math.floor(Math.random()*weatherCodes.length))]) {
@@ -145,19 +153,33 @@ function offlineCodeGen() { //offline debugging mode, also here in case person g
             drawSnow();
             break;
     }
-    //buttonGen();
-
+    buttonGen('celine',drawSunny);
+    buttonGen('fml', drawRainy);
+    buttonGen('another one', drawOvercast);
+    buttonGen('deejaay khALID', drawSnow);
 }
 
-function buttonGen() { //basically debugging function ig? need to implement buttons to change weather response or smth 
+function buttonGen(name,clickAction) { //basically debugging function ig? need to implement buttons to change weather response or smth 
+    const button = document.createElement('button');
+    const container = document.getElementById('tbContainer');
+    button.classList.add('testButton');
+    button.textContent = name;
+ //   button.onclick = clickAction;
+    container.appendChild(button);
+    button.onclick = clickAction;
+    
+}
 
+function testFunction() {
+    console.log('test 1')
+}
+function testFunction2() {
+    console.log('test 2')
 }
 
 function drawSunny() {
     console.log('you are my sunshine');
-
-    animateSprite('assets/mrsunnyshine.png',200,200,3,290,20,6);
-
+    animateSprite('assets/mrsunnyshine.png',200,200,3,canvas.width-250,canvas.height-700,6);
 }
 
 function drawRainy() {
@@ -176,27 +198,32 @@ function drawSnow() {
 
 }
 
-function animateSprite(spriteSrc, sW, sH, fN, posH, posW, EfN) { //running at 8-10 fps (ill figure it out), making function to animate them all so i just have to edit src bcz i respect myself.
+function animateSprite(spriteSrc, sW, sH, fN, posW, posH, EfN) { //running at 8-10 fps (ill figure it out), making function to animate them all so i just have to edit src bcz i respect myself.
     let img = new Image();
     img.src = spriteSrc;
     let animate;
-    canvas.addEventListener('mousemove', function(event){
+    let clicked = false;
+    canvas.addEventListener('mousedown', function(event){
         const rect = canvas.getBoundingClientRect();
         const mouseX = event.clientX - rect.left;
         const mouseY = event.clientY - rect.top;
-        hovering = (mouseX >= posX && mouseX <= posX + sW && mouseY >= posY && mouseY <= posY + sH);
+        if (mouseX >= posW && mouseX <= posW + sW && mouseY >= posH && mouseY <= posH + sH) {
+            clicked = !clicked
+            console.log('shits been clicked yahmean')
+        }
+
 
     });
+
     img.onload = function(){
         let cycle = 0;
-        let hovering = false;
         animate = setInterval(function(){
-            ctx.clearRect(0,0,sW,sH);
-            if (hovering) {
-                ctx.drawImage(img, cycle*sW, 0, sW, sH, posH, posW, sW, sH);
+            ctx.clearRect(posW,posH,sW,sH);
+            if (clicked) {
+                ctx.drawImage(img, cycle * sW, 0, sW, sH, posW, posH, sW, sH);
                 cycle = (cycle+1) % EfN;
             } else {
-                ctx.drawImage(img, cycle*sW, 0, sW, sH, posH, posW, sW, sH);
+                ctx.drawImage(img, cycle * sW, 0, sW, sH, posW, posH, sW, sH);
                 cycle = (cycle+1) % fN;
             }
         }, 110)
